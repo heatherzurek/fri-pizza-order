21d0b294db6e54d85271452cb5f26d289ba3382e
Heather Zurek <heather.zurek@gmail.com>
2019-04-12 08:34:02 -0700

    add pizzashop constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+//business logic for pizza shop
+function PizzaShop() {
+  this.pizzas = []
+}
+
+//global variable to save the pizzas inside
+
+PizzaShop.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+//this new method called addPizza(), takes a Pizza object as an argument
+//locates the PizzaShops pizzas array by calling this.pizzas
+//and uses push() to add the Pizza provided as an argument to the PizzaShops array property
+
+// business logic for pizza
 function Pizza(pizzaTopping, pizzaSize) {
   this.pizzaTopping = pizzaTopping;
   this.pizzaSize = pizzaSize;
