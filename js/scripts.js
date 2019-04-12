a35500a861dfb51ac6b38fd69ba44e30655a6625
Heather Zurek <heather.zurek@gmail.com>
2019-04-12 11:55:34 -0700

    add special requests logic and html

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,27 @@ PizzaShop.prototype.addPizza = function(pizza) {
 // }
 
 // BUSSINESS LOGIC FOR PIZZA
-function Pizza(pizzaTopping, pizzaSize) {
+function Pizza(pizzaTopping, pizzaSize, pizzaRequests) {
   this.pizzaTopping = pizzaTopping;
   this.pizzaSize = pizzaSize;
+  this.pizzaRequests = pizzaRequests;
+  // this.price = pizzaPrice;
 }
+
+// Pizza.prototype.sizeCalculator = function (inputSize) {
+//   this.pizzaSize = inputSize;
+// };
+//
+// Pizza.prototype.findPrice = function () {
+//   if(this.pizzaSize === "small") {
+//     this.price = 10;
+//   } else if (this.size === "medium") {
+//     this.price = 15;
+//   } else {
+//
+//   }
+//   return this.price;
+// }
 
 // Pizza.prototype.pizzaOrder = function() {
 //   return this.pizzaTopping + " " + this.pizzaSize;
@@ -32,16 +49,17 @@ var pizzaShop = new PizzaShop
 $(document).ready(function() {
   $("form#newOrder").submit(function(event) {
     event.preventDefault();
-    var inputtedPizzaTopping = $("input#newTopping").val();
-    var inputtedPizzaSize = $("input#newSize").val();
-    var size = $("input:radio[name=size]:checked").val();
+    var inputtedPizzaTopping = $("select#newTopping").val();
+    var selectedPizzaSize = $("select#newSize").val();
+    var inputtedPizzaRequests = $("input#pizzaRequests").val();
+    // var  = $("input:radio[name=size]:checked").val();
 
     // $("input#newTopping").val("");
     // $("input#newSize").val("");
-    var newPizza = new Pizza(inputtedPizzaTopping, inputtedPizzaSize);
+    var newPizza = new Pizza(inputtedPizzaTopping, selectedPizzaSize, inputtedPizzaRequests);
     pizzaShop.addPizza(newPizza);
 
-    $("#showOrder").text(newPizza.pizzaTopping + " " + newPizza.pizzaSize);
-    console.log(newPizza.pizza);
+    $("#showOrder").text(newPizza.pizzaSize + " " + newPizza.pizzaTopping + " " + newPizza.pizzaRequests);
+    console.log(newPizza);
   })
 })
