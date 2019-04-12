f488fb60b482eee2341508efdfbd748af52a1b36
Heather Zurek <heather.zurek@gmail.com>
2019-04-12 08:46:57 -0700

    add full scripts outline

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
 //business logic for pizza shop
+//global variable to save the pizzas inside
 function PizzaShop() {
-  this.pizzas = []
+  this.pizzas = [],
+  this.currentId = 0
 }
 
-//global variable to save the pizzas inside
-
 PizzaShop.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
   this.pizzas.push(pizza);
+}
+
+PizzaShop.prototype.assignId = function() {
+  this.currentId += 1;
+  retun this.currentId;
+}
+
+PizzaShop.prototype.findPizza = function(id) {
+  for (var i=0; i< this.pizzas.length; i++) {
+    if(this.pizzas[i]) {
+      if this.pizzas[i].id == id) {
+        return this.pizzas[i];
+      }
+    }
+  };
+  return false;
+}
+
+PizzaShop.prototype.deletePizza = function(id) {
+  for (var i=0; i< this.pizzas.length; i++) {
+    if (this.contacts[i]) {
+      if (this.pizzas[i].id == id) {
+        delete this.pizzas[i];
+          return true;
+      }
+    }
+  };
+  return false;
 }
 
 //this new method called addPizza(), takes a Pizza object as an argument
